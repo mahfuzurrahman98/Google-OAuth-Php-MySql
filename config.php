83d70870cda0496744a5be1f55b86a274c49b06a
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-07-18 18:37:36 +0600

    creds hidden

--- config.php
+++ config.php
@@ -8,8 +8,8 @@ function dd($data) {
 }
 
 // init configuration
-$clientID = '1052838296160-41g3863cei1a3uhbrpq7un7tsijbcea7.apps.googleusercontent.com';
-$clientSecret = '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';
+$clientID = '';
+$clientSecret = '';
 $redirectUri = 'http://127.0.0.1:8000/gauth-callback.php';
 
 // create Client Request to access Google API
